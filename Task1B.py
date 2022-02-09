b9491bfb3d8fd54a147b18c830e6fa4467bfbccd
DarkFlame618 <98828574+DarkFlame618@users.noreply.github.com>
2022-02-09 13:02:54 +0000

    Update Task1B.py

--- Task1B.py
+++ Task1B.py
@@ -1,5 +1,13 @@
-from floodsystem.geo import stations_by_distance
-from floodsytem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list
+pip install haversine
+from haversine import haversine
+def stations_by_distance(stations, p):
+  stationDistanceList = []
+  for i in stations:
+    distance = haversine(i.coord, p)
+    stationDistanceList.append(tuple([i, distance]))
+    stationDistanceList.sort(key=lambda x: x[1])
+  return stationDistanceList
 
 stations = build_station_list()
 
